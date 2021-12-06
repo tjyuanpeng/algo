4457c6f841df196963864af3a701f7719b0f9ab2
yuanpeng <yuanpeng@lenovocloud.com>
2021-12-06 10:02:11 +0800

    quick sort

---
+++ test/quick-sort.js
@@ -0,0 +1,14 @@
+/**
+
+ */
+
+var quickSort = function () {
+  return true
+}
+
+describe('快速排序', function () {
+  it('test', function () {
+    const r = quickSort()
+    r.should.to.equals(true)
+  })
+})
