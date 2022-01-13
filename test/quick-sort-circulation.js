49f7420afd34ce068bb5e37fa88253ef8f024d02
yuanpeng <yuanpeng@lenovocloud.com>
2022-01-13 13:04:13 +0800

    快速排序的两种方式

---
+++ test/quick-sort-circulation.js
@@ -0,0 +1,55 @@
+/**
+  快速排序 - 循环算法
+  https://www.jianshu.com/p/f5b157a974b0
+ */
+
+function quickSort(arr, left, right) {
+  var len = arr.length
+  var partitionIndex
+  var left = typeof left !== 'number' ? 0 : left
+  var right = typeof right !== 'number' ? len - 1 : right
+
+  if (left < right) {
+    partitionIndex = partition(arr, left, right)
+
+    quickSort(arr, left, partitionIndex - 1)
+    quickSort(arr, partitionIndex + 1, right)
+  }
+
+  return arr
+}
+
+function partition(arr, left, right) {
+  var pivot = left
+  var index = pivot + 1
+  for (var i = index; i <= right; i++) {
+    // 小于pivot的都放到一边
+    if (arr[i] < arr[pivot]) {
+      swap(arr, i, index)
+      index++
+    }
+  }
+
+  // 把pivot放到中间
+  // 整个数组从left到right都已经以pivot进行了分组
+  // 分组左边 < pivot，分组右边 >= pivot
+  swap(arr, pivot, index - 1)
+
+  return index - 1
+}
+
+function swap(arr, i, j) {
+  var temp = arr[i]
+  arr[i] = arr[j]
+  arr[j] = temp
+}
+
+describe('快速排序 - 循环算法', function () {
+  it('case 1', function () {
+    const r = quickSort([85, 24, 63, 45, 17, 31, 96, 50])
+    const anwser = [17, 24, 31, 45, 50, 63, 85, 96]
+    for (let i = 0; i < r.length; i++) {
+      r[i].should.to.equals(anwser[i])
+    }
+  })
+})
--- test/quick-sort.js
+++ test/quick-sort.js
@@ -1,14 +1,39 @@
 /**
-
+  快速排序
  */
 
-var quickSort = function () {
-  return true
+var quickSort = function (arr) {
+  if (arr.length <= 1) {
+    return arr
+  }
+
+  const less = []
+  const greater = []
+  const pivot = arr[0]
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] <= pivot) {
+      less.push(arr[i])
+    } else {
+      greater.push(arr[i])
+    }
+  }
+
+  return quickSort(less).concat(pivot).concat(quickSort(greater))
 }
 
 describe('快速排序', function () {
-  it('test', function () {
-    const r = quickSort()
-    r.should.to.equals(true)
+  it('case 1', function () {
+    const r = quickSort([85, 24, 63, 45, 17, 31, 96, 50])
+    const anwser = [17, 24, 31, 45, 50, 63, 85, 96]
+    for (let i = 0; i < r.length; i++) {
+      r[i].should.to.equals(anwser[i])
+    }
+  })
+  it('case 2', function () {
+    const r = quickSort([9, 8, 7, 6, 5, 4, 3, 2, 1])
+    const anwser = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    for (let i = 0; i < r.length; i++) {
+      r[i].should.to.equals(anwser[i])
+    }
   })
 })
